7a7dbcc3de4f97b25d1692c5d980d7504b2f3b22
olemoudi <olemoudi@gmail.com>
2016-03-08 02:48:43 +0100

    played with blocking channels

--- main.go
+++ main.go
@@ -4,39 +4,52 @@ import "fmt"
 import "net/http"
 import "os"
 import "bufio"
-
-func httpworker(id int, client *http.Client, requests <-chan *http.Request, responses chan<- *http.Response) {
-	for req := range requests {
-		fmt.Println("worker", id, "downloading url", req.URL)
-		resp, err := client.Do(req)
-		if err != nil {
-			// handle error
-			fmt.Println("worker", id, "- Error downloading url", req.URL)
-			continue
-		}
-		responses <- resp
-	}
-}
+import "time"
+import "sync/atomic"
 
 func main() {
 
+	var reqs int64 = 0
+
 	httpreqs := make(chan *http.Request, 100)
-	httpresps := make(chan *http.Response, 100)
-
-	client := &http.Client{}
-
-	for w := 1; w <= 20; w++ {
-		go httpworker(w, client, httpreqs, httpresps)
+	//httpresps := make(chan *http.Response, 100)
+	done := make(chan bool)
+	timeout := time.Duration(5 * time.Second)
+	client := &http.Client{
+		Timeout: timeout,
 	}
 
-	inFile, _ := os.Open("alexa.txt")
+	for w := 1; w <= 20; w++ {
+		go func(id int) {
+			for {
+				req, more := <-httpreqs
+				if more {
+					atomic.AddInt64(&reqs, 1)
+					fmt.Println("worker", id, "downloading url", reqs, req.URL)
+					_, err := client.Do(req)
+					if err != nil {
+						// handle error
+						fmt.Println("worker", id, "- Error downloading url", reqs, req.URL)
+						continue
+					}
+				} else {
+					done <- true
+					return
+				}
+				//httpresps <- resp
+			}
+		}(w)
+	}
+
+	inFile, _ := os.Open(os.Args[1])
 	defer inFile.Close()
 	scanner := bufio.NewScanner(inFile)
 	scanner.Split(bufio.ScanLines)
 	for scanner.Scan() {
-		req, _ := http.NewRequest("GET", scanner.Text(), nil)
+		// req, _ := http.NewRequest("GET", scanner.Text(), nil)
+		req, _ := http.NewRequest("GET", "http://mediavida.com", nil)
 		httpreqs <- req
 	}
 	close(httpreqs)
-	<-httpresps
+	<-done
 }
